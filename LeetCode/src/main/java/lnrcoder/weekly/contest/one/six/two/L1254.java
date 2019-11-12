6bc354ad166f87c0af3b83dcd40143b9b77152a6
lnrCoder <shlycsd@163.com>
2019-11-12 16:01:01 +0800

    周赛 162 第三题 1254

--- LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/two/L1254.java
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/two/L1254.java
@@ -18,6 +18,15 @@ public class L1254 {
     private static boolean mark;
     private static int[][] rowCol = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
+    /**
+     * 有一个二维矩阵 grid ，每个位置要么是陆地（记号为 0 ）要么是水域（记号为 1 ）。
+     * 我们从一块陆地出发，每次可以往上下左右 4 个方向相邻区域走，能走到的所有陆地区域，我们将其称为一座「岛屿」。
+     * 如果一座岛屿 完全 由水域包围，即陆地边缘上下左右所有相邻区域都是水域，那么我们将其称为 「封闭岛屿」。
+     * 请返回封闭岛屿的数目。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/number-of-closed-islands
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public static int closedIsland(int[][] grid) {
         int n = grid.length;
         if (n <= 2) {
