78a682a80c62e0d2a1e5d1acc2f0d18ecf3763b9
lnrCoder <shlycsd@163.com>
2019-11-19 11:13:52 +0800

    周赛 163 第二题 1261

--- LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/three/L1261.java
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/three/L1261.java
@@ -1,5 +1,7 @@
 package lnrcoder.weekly.contest.one.six.three;
 
+import java.util.HashSet;
+
 /**
  * @ClassName L1261
  * @description 在受污染的二叉树中查找元素
@@ -8,5 +10,35 @@ package lnrcoder.weekly.contest.one.six.three;
  */
 public class L1261 {
 
+    HashSet<Integer> set;
 
+    public L1261(TreeNode root) {
+        dfs(root, 0);
+    }
+
+    public void dfs(TreeNode root, int val) {
+        if (root==null) {
+            return;
+        }
+        root.val = val;
+        set.add(val);
+        dfs(root.left, val * 2 + 1);
+        dfs(root.right, val * 2 + 2);
+    }
+
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+
+}
+
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
 }
