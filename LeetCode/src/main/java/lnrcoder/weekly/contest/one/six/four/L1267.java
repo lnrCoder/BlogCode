462a3cb1b7e2b6e669f06890f91d4c2fd6a387d0
lnrCoder <shlycsd@163.com>
2019-11-26 15:05:17 +0800

    周赛 164 第二题 统计参与通信的服务器

--- LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/four/L1267.java
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/four/L1267.java
@@ -19,6 +19,14 @@ public class L1267 {
         System.out.println("l1267.countServers(grid) = " + l1267.countServers(grid));
     }
 
+    /**
+     * 这里有一幅服务器分布图，服务器的位置标识在 m * n 的整数矩阵网格 grid 中，1 表示单元格上有服务器，0 表示没有。
+     * 如果两台服务器位于同一行或者同一列，我们就认为它们之间可以进行通信。
+     * 请你统计并返回能够与至少一台其他服务器进行通信的服务器的数量。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/count-servers-that-communicate
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public int countServers(int[][] grid) {
 
         Map<Integer, Integer> mapx = new HashMap<>();
