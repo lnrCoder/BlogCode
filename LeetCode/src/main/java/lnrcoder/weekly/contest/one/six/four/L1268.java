263a50c1a6fae521db38ff3ab971194a477b1871
lnrCoder <shlycsd@163.com>
2019-11-26 17:16:14 +0800

    周赛 164 第三题 搜索推荐系统

--- LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/four/L1268.java
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/four/L1268.java
@@ -20,6 +20,14 @@ public class L1268 {
                 "l1268.suggestedProducts(products,searchWord) = " + l1268.suggestedProducts(products, searchWord));
     }
 
+    /**
+     * 给你一个产品数组 products 和一个字符串 searchWord ，products  数组中每个产品都是一个字符串。
+     * 请你设计一个推荐系统，在依次输入单词 searchWord 的每一个字母后，推荐 products 数组中前缀与 searchWord 相同的最多三个产品。如果前缀相同的可推荐产品超过三个，请按字典序返回最小的三个。
+     * 请你以二维列表的形式，返回在输入 searchWord 每个字母后相应的推荐产品的列表。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/search-suggestions-system
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         List<List<String>> result = new ArrayList<>(searchWord.length());
         List<String> list = new ArrayList<>(products.length);
