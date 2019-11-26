da71ca36453f2cca4776eca2fdd1c6651d780a4d
lnrCoder <shlycsd@163.com>
2019-11-26 17:17:48 +0800

    周赛 164 第四题 停在原地的方案数

---
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/four/L1269.java
@@ -0,0 +1,22 @@
+package lnrcoder.weekly.contest.one.six.four;
+
+/**
+ * @ClassName L1269
+ * @description 停在原地的方案数
+ * @Author LiaNg
+ * @Date 2019-11-26
+ */
+public class L1269 {
+
+    public static void main(String[] args) {
+        int steps = 3;
+        int arrLen = 2;
+        L1269 l1269 = new L1269();
+        System.out.println("l1269.numWays(steps,arrLen) = " + l1269.numWays(steps, arrLen));
+    }
+
+    public int numWays(int steps, int arrLen) {
+        return 1;
+    }
+
+}
