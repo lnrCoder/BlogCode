2b480471bbaebcac96c9d79ed5d9b22ab9871a03
lnrCoder <shlycsd@163.com>
2019-11-26 14:32:54 +0800

    周赛 164 第一题 访问所有点的最小时间

--- LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/four/L1266.java
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/four/L1266.java
@@ -14,6 +14,15 @@ public class L1266 {
         System.out.println("l1266.minTimeToVisitAllPoints(points) = " + l1266.minTimeToVisitAllPoints(points));
     }
 
+    /**
+     * 平面上有 n 个点，点的位置用整数坐标表示 points[i] = [xi, yi]。请你计算访问所有这些点需要的最小时间（以秒为单位）。
+     * 你可以按照下面的规则在平面上移动：
+     * 每一秒沿水平或者竖直方向移动一个单位长度，或者跨过对角线（可以看作在一秒内向水平和竖直方向各移动一个单位长度）。
+     * 必须按照数组中出现的顺序来访问这些点。
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/minimum-time-visiting-all-points
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public int minTimeToVisitAllPoints(int[][] points) {
 
         int result = 0;
@@ -21,7 +30,6 @@ public class L1266 {
         for (int i = 1; i < points.length; i++) {
             int x = Math.abs(points[i][0] - points[i - 1][0]);
             int y = Math.abs(points[i][1] - points[i - 1][1]);
-
             result += Math.max(x, y);
 
         }
