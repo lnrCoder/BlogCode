75da03e4e09ee1a198f6418ef5d878856b5b85c6
lnrCoder <shlycsd@163.com>
2019-12-01 17:46:09 +0800

    周赛 165

--- LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/five/Weekly.java
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/five/Weekly.java
@@ -34,7 +34,7 @@ public class Weekly {
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if()
+
             }
         }
     }
