48392a0fe1fb8d6f96af25e2208398fcd1ca26b4
lnrCoder <shlycsd@163.com>
2019-12-01 10:22:03 +0800

    周赛 165

---
+++ LeetCode/src/main/java/lnrcoder/weekly/contest/one/six/five/Weekly.java
@@ -0,0 +1,19 @@
+package lnrcoder.weekly.contest.one.six.five;
+
+/**
+ * @ClassName Weekly
+ * @description 165周赛
+ * @Author LiaNg
+ * @Date 2019-12-01
+ */
+public class Weekly {
+
+    public static void main(String[] args) {
+
+        Weekly weekly = new Weekly();
+
+
+    }
+
+
+}
