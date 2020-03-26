70b2e62eac42827c3ac4d87355e91fe1cfdd7a46
lnrcoder <shlycsd@163.com>
2020-03-26 22:47:53 +0800

    线程的创建于运行

---
+++ JAVA-JUC/src/main/java/Thread/MyThread.java
@@ -0,0 +1,59 @@
+package Thread;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @ClassName: MyThread
+ * @Description:
+ * @Author: LiaNg
+ * @Date: 2020/3/26 22:03
+ */
+public class MyThread {
+
+    public static void main(String[] args) {
+        TestThread thread = new TestThread();
+        thread.start();
+
+        TestRunable testRunable = new TestRunable();
+        testRunable.run();
+
+        FutureTask<String> testFuture = new FutureTask<>(new CallerTask());
+        new Thread(testFuture).start();
+        try {
+            System.out.println("testFuture.get() = " + testFuture.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 方式一： 通过 Thread 类
+     */
+    public static class TestThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println(" -- 通过集成 Thread 运行线程 -- ");
+        }
+    }
+
+    /**
+     * 方式二： 通过实现 Runnable 接口
+     */
+    public static class TestRunable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(" -- 通过 Runable 运行线程-- ");
+        }
+    }
+
+    /**
+     * 方式三： 通过 FutureTask 异步调用
+     */
+    public static class CallerTask implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            return "Hello World !";
+        }
+    }
+}
